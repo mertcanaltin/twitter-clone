123bece3091d68665d37ad425dc759c858b11bdd
m <m@ms-MacBook-Pro.local>
2021-05-22 22:16:34 +0300

    final

---
+++ src/components/TweetBox.js
@@ -0,0 +1,69 @@
+import firebase from "firebase";
+import React, { useState } from "react";
+import db from "../firebase";
+import {
+  EmojiIcon,
+  GIFIcon,
+  ImageIcon,
+  PollIcon,
+  ScheduleIcon,
+} from "../icons/Icon";
+
+const TweetBox = () => {
+  const [content, setContent] = useState("");
+
+  const sendTweet = () => {
+    if (content !== "") {
+      db.collection("feed").add({
+        displayName: "mert can altın",
+        username: "@mecaltin",
+        content,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        image:
+          "https://pbs.twimg.com/media/E19BiZWWEAM9hjq?format=jpg&name=4096x4096",
+        avatar:
+          "https://pbs.twimg.com/profile_images/1393509283699036162/-N6uX0Yc_400x400.jpg",
+      });
+
+      setContent("");
+    }
+  };
+
+  return (
+    <div className="flex flex-col flex-1 mt-2 px-2">
+      <textarea
+        className="w-full text-xl placeholder-gray-dark outline-none overflow-hidden resize-none bg-transparent"
+        placeholder="What's happening?"
+        onChange={(e) => setContent(e.target.value)}
+        value={content}
+      />
+      <div className="flex items-center justify-between">
+        <div className="flex -ml-3">
+          <div className="flex items-center justify-center w-11 h-11 rounded-full hover:bg-gray-lightest">
+            <ImageIcon className="w-6 h-6 text-primary-base" />
+          </div>
+          <div className="flex items-center justify-center w-11 h-11 rounded-full hover:bg-gray-lightest">
+            <GIFIcon className="w-6 h-6 text-primary-base" />
+          </div>
+          <div className="flex items-center justify-center w-11 h-11 rounded-full hover:bg-gray-lightest">
+            <PollIcon className="w-6 h-6 text-primary-base" />
+          </div>
+          <div className="flex items-center justify-center w-11 h-11 rounded-full hover:bg-gray-lightest">
+            <EmojiIcon className="w-6 h-6 text-primary-base" />
+          </div>
+          <div className="flex items-center justify-center w-11 h-11 rounded-full hover:bg-gray-lightest">
+            <ScheduleIcon className="w-6 h-6 text-primary-base" />
+          </div>
+        </div>
+        <button
+          className="bg-primary-base text-white rounded-full px-4 py-2 font-medium"
+          onClick={sendTweet}
+        >
+          Tweet
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default TweetBox;
